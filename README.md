02167828c32612276f210b7e944a60ff693a6e68
satish <satishlariya@gmail.com>
2022-11-03 23:32:46 +0530

    added changes for consumer

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# redis-stream-consumer
-basic app to which consumes message from redis streams
+# Redis Streams
+basic app to show redis streams operations 
+- steps to setup redis server locally
+ 1. download redis server for windows - https://github.com/tporadowski/redis/releases/download/v5.0.14.1/Redis-x64-5.0.14.1.zip
---
+++ src/main/java/com/sample/redis/consumer/VideoEventConsumer.java
@@ -0,0 +1,38 @@
+package com.sample.redis.consumer;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.stream.ObjectRecord;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.stream.StreamListener;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.net.InetAddress;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Service
+@Slf4j
+public class VideoEventConsumer implements StreamListener<String, ObjectRecord<String, String>> {
+
+    private AtomicInteger atomicInteger = new AtomicInteger(0);
+
+    @Autowired
+    private ReactiveRedisTemplate<String, String> redisTemplate;
+
+    @Override
+    @SneakyThrows
+    public void onMessage(ObjectRecord<String, String> record) {
+        log.info(InetAddress.getLocalHost().getHostName() + " - consumed :" + record.getValue());
+
+        atomicInteger.incrementAndGet();
+    }
+
+    @Scheduled(fixedRate = 10000)
+    public void showPublishedEventsSoFar() {
+        log.info("Total Consumer :: " + atomicInteger.get());
+    }
+
+}
